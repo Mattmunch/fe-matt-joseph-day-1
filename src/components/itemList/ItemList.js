753d0bbddccc2d39b634801b6f72143b086ad3fe
Matt Munch <mattmunch94@gmail.com>
2020-02-17 14:33:41 -0800

    item list

--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import useItems from '../../hooks/useItems';
+import Item from '../item/Item';
 
 const ItemList = () => {
-    const items = useItems();
-    const itemElements = 
+  const items = useItems();
+  const itemElements = items.map(({ name, expirationDate }, i) => (
+    <li key={i}>
+      <Item name={name} expirationDate={expirationDate} />
+    </li>
+  ));
   return (
     <>
+      {itemElements}
     </>
   );
 };
